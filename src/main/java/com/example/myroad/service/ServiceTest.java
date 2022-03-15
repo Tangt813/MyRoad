b6e2a81d533cb16860d8bdd3650a6b0a40a95a8a
tangt813201 <78403006+Tangt813@users.noreply.github.com>
2022-03-15 13:39:45 +0800

    上传test

---
+++ src/main/java/com/example/myroad/service/ServiceTest.java
@@ -0,0 +1,11 @@
+package com.example.myroad.service;
+
+/**
+ * @ClassName: ServiceTest
+ * @Description: TODO
+ * @Author: Tangt
+ * @Date: 2022/3/15 13:32
+ * @Version: v1.0
+ */
+public class ServiceTest {
+}
